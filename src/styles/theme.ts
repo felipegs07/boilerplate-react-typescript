0d573f81b12c811106a97529cdbdd9c4e1b4a173
Felipe Gustavo <felipegdas07@gmail.com>
2021-02-02 10:58:15 -0300

    adding theme provider structure into the boilerplate

---
+++ src/styles/theme.ts
@@ -0,0 +1,8 @@
+export default {
+  font: {
+    family: {
+      default:
+        "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+    },
+  },
+};
